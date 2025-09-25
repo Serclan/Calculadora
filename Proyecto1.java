e64aa5188a6d9edbc771d7dbc762d3f1c0cc9cd7
Serclan <sergio.garciaramiro@gmail.com>
2025-09-25 14:07:41 +0200

    Add files via upload

---
+++ Proyecto1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1;
+
+/**
+ *
+ * @author sergi
+ */
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+        v.setVisible(true);
+        
+    }
+}
